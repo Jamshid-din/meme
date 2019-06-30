0b715a27a43eb2482519264b7b92f822ae604741
Jamshid Ismailov <jamshid@Jamshids-MacBook-Pro.local>
2019-07-01 03:23:20 +0500

    Updates

---
+++ precache-manifest.3a0d1fe7e5673114fd80a225075aeca4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ee6490931814cad24b8be5af7507d4",
+    "url": "/meme.github.io/index.html"
+  },
+  {
+    "revision": "369d6397badfd32a4acb",
+    "url": "/meme.github.io/static/css/main.ee220ad0.chunk.css"
+  },
+  {
+    "revision": "6a3e563be3203583492c",
+    "url": "/meme.github.io/static/js/2.561c52a4.chunk.js"
+  },
+  {
+    "revision": "369d6397badfd32a4acb",
+    "url": "/meme.github.io/static/js/main.37316cdf.chunk.js"
+  },
+  {
+    "revision": "2e17c5701b1957440ce6",
+    "url": "/meme.github.io/static/js/runtime~main.1ea199c3.js"
+  },
+  {
+    "revision": "be324d5b695be0894732e4e02e8fcc9f",
+    "url": "/meme.github.io/static/media/meme_logo.be324d5b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todolist.github.io/precache-manifest.62f6a40ab086fd334ecac955f14face8.js"
+  "/meme.github.io/precache-manifest.3a0d1fe7e5673114fd80a225075aeca4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todolist.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/meme.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
