6574399ec3017e9c9ae5c27c373f2e332215cfa9
Jamshid Ismailov <jamshid@Jamshids-MacBook-Pro.local>
2019-07-01 03:07:40 +0500

    Updates

---
+++ precache-manifest.82e2948d07f81106e6894c39cfcc2a18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84a5862a50efb6f931ed2b14013de15",
+    "url": "/meme/index.html"
+  },
+  {
+    "revision": "74cc9ef1541ae2774ec2",
+    "url": "/meme/static/css/main.ee220ad0.chunk.css"
+  },
+  {
+    "revision": "6a3e563be3203583492c",
+    "url": "/meme/static/js/2.561c52a4.chunk.js"
+  },
+  {
+    "revision": "74cc9ef1541ae2774ec2",
+    "url": "/meme/static/js/main.062e8b3e.chunk.js"
+  },
+  {
+    "revision": "41968b294926a2d87494",
+    "url": "/meme/static/js/runtime~main.1c7c63e9.js"
+  },
+  {
+    "revision": "be324d5b695be0894732e4e02e8fcc9f",
+    "url": "/meme/static/media/meme_logo.be324d5b.png"
+  }
+]);
